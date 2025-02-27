9c32afc36d4a46efa860156d68c95e23fac36ce4
Supriya Kulkarni <2004supriya.kulkarni@gmail.com>
2025-02-27 11:51:34 +0530

    added initial buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css'
+import "./App.css";
+import buttonStyles from "./components/buttons/ButtonStyles";
 
 function App() {
-
   return (
     <>
-    Hello
+      <div className="absolute top-0 left-0 p-4">
+        <button
+          onClick={() => {}}
+          className={buttonStyles.editEmergencyContacts}
+        >
+          Edit Emergency Contacts
+        </button>
+      </div>
+
+      <div className="flex items-center justify-center h-screen">
+        <button
+          onClick={() => {}}
+          className={buttonStyles.emergencyButton}
+        >
+          EMERGENCY
+        </button>
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full flex items-center justify-center gap-4 p-6">
+        <button
+          onClick={() => {}}
+          className={buttonStyles.editEmergencyContacts}
+        >
+          Report Unsafe Location
+        </button>
+
+        <button
+          onClick={() => {}}
+          className={buttonStyles.editEmergencyContacts}
+        >
+          Find Unsafe Location Near Me
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
